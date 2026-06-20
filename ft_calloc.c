4a5d6d7f26fdc162f37be2df73dda47b0210c314
Ula Kaminska <s28722@pjwstk.edu.pl>
2026-06-20 08:11:18 +0200

    refactor: change return val when arg is 0

--- ft_calloc.c
+++ ft_calloc.c
@@ -2,8 +2,8 @@
 ** The calloc() function allocates memory for an array of nmemb elements
 ** of size bytes each and returns a pointer to the allocated memory.
 ** The memory is set to zero. If nmemb or size is 0,
-** then calloc() returns either NULL,
-** or a unique pointer value that can later be successfully passed to free().
+** then calloc() returns a unique pointer value
+** that can later be successfully passed to free().
 */
 
 #include "libft.h"
@@ -13,6 +13,8 @@ void	*ft_calloc(size_t nelem, size_t elsize)
 	void	*dst;
 
 	if (!nelem || !elsize)
+		return (malloc(0));
+	if (nelem > ((size_t)-1) / elsize)
 		return (NULL);
 	dst = (void *)malloc(elsize * nelem);
 	if (!dst)
